9728187550cc6b299ee916b5a3f594d7af5440d5
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-10-13 10:13:43 -0400

    prepped for staging

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const db = require('../models');
 
-mongoose.connect('mongodb://localhost/workout', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
   useFindAndModify: false,
+  useCreateIndex: true,
   useUnifiedTopology: true,
 });
 
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
@@ -11,11 +12,11 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/quiet-woodland", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
 })
 
 app.use(require('./routes/api'))
