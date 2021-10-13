768bea6c7d74d548e3f6ff8d33fb370575d283f2
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-10-12 21:22:19 -0400

    env variables

--- server.js
+++ server.js
@@ -11,10 +11,11 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/quiet-woodland", {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     useCreateIndex: true,
+    useFindAndModify: false
 })
 
 app.use(require('./routes/api'))
