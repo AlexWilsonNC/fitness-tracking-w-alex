db2c4350b07a1f37044d958a6897a338075e7847
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-10-14 09:26:06 -0400

    removed extra file

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const apiRoutes = require("./api.js");
-const homeRoutes = require("./paths.js");
-
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-
-module.exports = router;
